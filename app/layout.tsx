bc52520c4524e7bdfba1e451ee0ce7be622f353b
methodkopilka <seitkularman@gmail.com>
2025-12-04 09:03:33 +0500

    3

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from '@/components/ThemeProvider'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'StudyHub - Методическая копилка',
+  title: 'MethodX - Методическая копилка',
   description: 'Платформа для обмена учебными материалами',
 }
 
